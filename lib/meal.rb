afe9129863946c672b158b6d5f755c6a6e99155e
GoTo15 <github email address>
2020-01-30 14:18:03 -0600

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,20 @@
 class Meal
 
+    attr_reader :customer, :tip, :total, :waiter
+
+    @@all = []
+
+    def initialize(waiter, customer, total, tip)
+        @customer = customer
+        @tip = tip
+        @total = total
+        @waiter = waiter
+
+        @@all << self
+    end
+
+    # Class Method
+    def self.all
+        @@all
+    end
 end
