a3504f041157c1c13fcb497c41bb7ee6eee10c09
mystical-calculator-5374 <bbrower@smu.edu>
2020-02-02 22:08:00 +0000

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,33 @@
 class Waiter
+  attr_accessor :name, :experience
+
+  @@all = []
+
+  def initialize(name, experience)
+    @name = name
+    @experience = experience
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(customer, total, tip)
+    Meal.new(self, customer, total, tip)
+  end
+
+  def meals
+    Meal.all.select { |meal| meal.waiter == self }
+  end
+
+  def customers
+    self.meals.map { |meal| meal.customer }
+  end
+
+  def best_tipper
+    self.meals.max_by { |meal| meal.tip }.customer
+  end
 
 end
